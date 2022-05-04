f585dc8ad64251e53abf3ca352dbba310a292433
devshum <57689318+devshum@users.noreply.github.com>
2022-05-04 17:05:27 +0300

    one root state

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -8,7 +8,7 @@ import { NgForm } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
 import * as ShoppingListActions from '../store/shopping-list.actions'
-import * as fromShoppingList from '../store/shopping-list.reducer';
+import * as fromApp from '../../store/app.reducer';
 
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
@@ -27,7 +27,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
 
   constructor(
     private slService: ShoppingListService,
-    private store: Store<fromShoppingList.AppState> 
+    private store: Store<fromApp.AppState> 
   ) { }
 
   ngOnInit() {
