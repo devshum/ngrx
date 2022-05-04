35708d3b3e6cab3ecab2c43d82e49a7a4755c709
devshum <57689318+devshum@users.noreply.github.com>
2022-05-04 12:29:56 +0300

    set up reducers

---
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -0,0 +1,76 @@
+import { Ingredient } from "../../shared/ingredient.model";
+import * as ShoppingListActions from "./shopping-list.actions";
+
+export interface State {
+  ingredients: Ingredient[],
+  editedIngredient: Ingredient,
+  editedIngredientIndex: number
+}
+
+export interface AppState {
+  shoppingList: State
+}
+
+const initialState: State = {
+  ingredients: [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10)
+  ],
+  editedIngredient: null,
+  editedIngredientIndex: -1
+}
+
+export function shoppingListReducer(state: State = initialState, action: ShoppingListActions.ShoppingListActions) {
+  switch(action.type) {
+    case ShoppingListActions.ADD_INGREDIENT:
+      return {
+        ...state,
+        ingredients: [...state.ingredients, action.payload]
+      }
+    ///
+    case ShoppingListActions.ADD_INGREDIENTS:
+      return {
+        ...state,
+        ingredients: [...state.ingredients, ...action.payload]
+      }
+    ///
+    case ShoppingListActions.UPDATE_INGREDIENT:
+      const ingredient = state.ingredients[action.payload.index]
+      const updateIngredient = {
+        // copy old ingredient
+        ...ingredient,
+        // copy new ingredient and overwtire old ingredient
+        ...action.payload.ingredient
+      }
+      const updatedIngredients = [...state.ingredients]
+      updatedIngredients[action.payload.index] = updateIngredient;
+
+      return {
+        ...state,
+        ingredients: updatedIngredients
+      }
+    ///
+    case ShoppingListActions.DELETE_INGREDIENT:
+      return {
+        ...state,
+        ingredients: state.ingredients.filter((_, index) => index !== action.payload)
+      }
+    ///
+    case ShoppingListActions.START_EDIT:  
+      return {
+        ...state,
+        editedIngredientIndex: action.payload,
+        editedIngredient: { ...state.ingredients[action.payload] }
+      }
+    ///
+    case ShoppingListActions.STOP_EDIT:
+      return {
+        ...state,
+        editedIngredientIndex: -1,
+        editedIngredient: null
+      }
+    ///
+    default: 
+      return state
+  }
+}
